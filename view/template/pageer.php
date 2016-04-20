c49e104d89edc6b1a96e9fd3dc89d31a4a386d89
zhoujunlong <zhoujunlong@antiy.cn>
2016-04-20 14:40:57 +0800

    完成文章管理

---
+++ view/template/pageer.php
@@ -0,0 +1,43 @@
+<nav style="margin-top: -20px">
+    <ul class="pagination pagination-sm">
+        <?php
+        $pages=Request::get('pages');
+        $page=Request::get('page');
+        $from=1;
+        $to=$pages;
+        if($page>7){
+            $from=$page-7;
+        }
+        if($pages-$page>7){
+            $to=$page+7;
+        }
+        $from=$to-14>0?$to-14:1;
+        $to=$from+14>$pages?$pages:$from+14;
+        ?>
+
+        <!-- 上一页 -->
+        <?php if($page!=1): ?>
+            <li><a href="./?p=1">首页</a></li>
+            <li ><a href="./?p=<?php echo  $page-1?>"><span>&laquo;</span></a></li>
+        <?php endif;?>
+        <?php if($page==1): ?>
+            <li class="disabled"><a>首页</a></li>
+            <li class="disabled"><span>&laquo;</span></li>
+        <?php endif;?>
+
+        <!-- 所有分页，最多显示15个分页数目 -->
+        <?php for($i=$from;$i<=$to;$i++): ?>
+            <li <?php if($i== $page) echo "class='active'"; ?>><a href="./?p=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+        <?php endfor; ?>
+
+        <!-- 下一页 -->
+        <?php if($page!= $pages): ?>
+            <li><a href="./?p=<?php echo $page+1?>"><span>&raquo;</span></a></li>
+            <li><a href="./?p=<?php echo $pages?>">尾页</a></li>
+        <?php endif;?>
+        <?php if($page==$pages): ?>
+            <li class="disabled"><span>&raquo;</span></li>
+            <li class="disabled"><a>尾页</a></li>
+        <?php endif;?>
+    </ul>
+</nav>
