4e7819713b7ed6728b53a0bcb88539ff421a6fd4
zhoujunlong <zhoujunlong@antiy.cn>
2016-04-21 21:03:57 +0800

    使用web app

--- view/index.view.php
+++ view/index.view.php
@@ -15,8 +15,9 @@
     <!-- 首页page -->
     <div class="page page-current">
         <header class="bar bar-nav">
-            <a class="icon icon-me pull-left open-panel"></a>
+            <a class="icon icon-me open-panel pull-left" data-panel='#uesrpanel'></a>
             <h1 class="title" id="maintitle">Xnews - 头条</h1>
+            <a class="icon icon-me open-panel pull-right" data-panel='#tagpanel'></a>
         </header>
         <div class="content">
             <div class="content-block" id="newslist">
@@ -24,14 +25,22 @@
             </div>
         </div>
     </div>
-
 </div>
 
-<div class="panel panel-left panel-reveal">
-    <div class="content-block" id="taglist">
+<!-- 用户中心 -->
+<div class="panel panel-left panel-reveal" id="uesrpanel">
+    <div class="content-block">
+        <p>用户中心</p>
+        <p></p>
+        <p><a class="close-panel">关闭</a></p>
+    </div>
+</div>
+
+<!-- 分类选择 -->
+<div class="panel panel-right panel-reveal" id="tagpanel">
+    <div class="content-block">
         <p>分类选择</p>
         <p></p>
-        <!-- Click on link with "close-panel" class will close panel -->
         <p><a class="close-panel">关闭</a></p>
     </div>
 </div>
